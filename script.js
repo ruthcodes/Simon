c4079dbbfcff6b033b37194fcee32699e78f9ce2
Ruth Ashleigh <notruth@notruth.com>
2018-09-12 22:21:20 -0700

    Rough outlines for async move selections

--- script.js
+++ script.js
@@ -4,13 +4,26 @@ document.addEventListener("DOMContentLoaded", function(event) {
   let simonsMoves = [];
   let gameOn = false;
   let strictMode = false;
+  let hasClicked = false;
 
   document.querySelectorAll('.colour').forEach(colour => {
-    colour.addEventListener("click", (e) => {
-      usersMoves.push(e.target.id);
-      flash(e.target.id);
-    })
+    colour.addEventListener("click", (e) => selectMove(e))
   })
+
+  const selectMove = async (e) => {
+    hasClicked = true;
+    clearTimeout(waitForUser);
+    usersMoves.push(e.target.id);
+    await flash(e.target.id);
+    console.log(usersMoves)
+    console.log(simonsMoves)
+    if (usersMoves.length < simonsMoves.length){
+      usersTurn()
+    } else {
+      usersMoves = [];
+      play()
+    }
+  }
 
   document.querySelector('input').addEventListener("click", () => {
     gameOn = !gameOn;
@@ -29,12 +42,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
     const colours = ["red", "yellow", "green", "blue"];
     let move = colours[Math.floor(Math.random()*colours.length)];
     simonsMoves.push(move);
-    console.log(simonsMoves);
+    //console.log(simonsMoves);
   }
 
-  document.querySelector('#start').addEventListener("click", () => chooseSimonsMoves())
+  document.querySelector('#start').addEventListener("click", () => play())
 
-  const flash = async (colour) => {
+  const flash = async (colour, speed) => {
     if (gameOn){
       switch(colour) {
         case "red":
@@ -56,20 +69,71 @@ document.addEventListener("DOMContentLoaded", function(event) {
       }
       let audio = new Audio(file);
       document.getElementById(colour).style.background = colour;
+      //flash colour and play sound simultaneously
+      setTimeout(() => {document.getElementById(colour).style.background = defaultColor}, 100)
       audio.play();
-      await new Promise((resolve) => setTimeout(() => {
-        document.getElementById(colour).style.background = defaultColor;
-        resolve();
-      }, 500));
+      await new Promise((resolve) => setTimeout(() => {resolve()}, speed));
+    }
+  }
+
+  const playSimonsMoves = async () => {
+    for (move in simonsMoves){
+      await flash(simonsMoves[move], 800)
     }
   }
 
   const turnOn = async () => {
-    await flash("red");
-    await flash("blue");
-    await flash("yellow");
-    await flash("green");
+    await flash("green", 300);
+    await flash("red", 300);
+    await flash("blue", 300);
+    await flash("yellow", 300);
   }
 
+  const play = async () => {
+    chooseSimonsMoves();
+    await playSimonsMoves();
+    usersTurn()
+  }
 
+  const usersTurn = async () => {
+    hasClicked = false;
+    // wait 2 seconds for user to enter move
+    waitForUser = setTimeout(()=> {
+      wrongMove();
+    }, 2000)
+
+    /*await new Promise((resolve) => setTimeout(() => {
+      resolve();
+    }, 2000));
+    // if userMoves within time,
+    if (hasClicked){
+      //clear the timeout from above
+      console.log("they clicked")
+      //console.log(`usersMoves: ${usersMoves.length}, simonsMoves: ${simonsMoves.length}`)
+      if (usersMoves.length < simonsMoves.length){
+        console.log("It's still the users turn, calling it again")
+        //console.log(`usersMoves: ${usersMoves.length}, simonsMoves: ${simonsMoves.length}`)
+        usersTurn();
+      } else {
+        console.log("computers move")
+        usersMoves = [];
+        play()
+      }
+    } else {
+      wrongMove()
+    }
+    */
+
+
+
+    // compare it to same index in simonsMoves
+  }
+
+  const wrongMove = () => {
+    console.log("wrong!")
+  }
+
+// if userMoves within time, compare it to same index in simonsMoves
+// else, wrong
+// if it's the last of simons moves, start again
 });
